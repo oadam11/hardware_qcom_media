bb82da4c4cf163b14fe0b651a06601bb7e1023e4
oadam11 <ovadia.adam@gmail.com>
2017-09-30 11:20:34 -0400

    disable for now

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ QCOM_MEDIA_ROOT := $(call my-dir)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 
-include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
+#include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
 include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 
 ifeq ($(TARGET_BOARD_PLATFORM),apq8084)
